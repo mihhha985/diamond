edd419400b082da8f9228d518949315efc760c7e
Mikhail Biketov <mihhha985@yandex.ru>
2023-12-16 17:57:29 +0300

    add remote patterns

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-		domains:['localhost'],
+    remotePatterns: [
+      {
+      	protocol: process.env.API_PROTOCOL,
+        hostname: process.env.API_HOST,
+        port: `${process.env.API_PORT}`,
+      },
+			
+    ],
   },
 	env:{
 		serverUrl:`${process.env.API_PROTOCOL}://${process.env.API_HOST}:${process.env.API_PORT}`,
